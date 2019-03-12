741086ff83081fd845d97bbbdf0b426f18562cbc
Meghea Iulian <iulian.meghea@gmail.com>
2019-03-12 14:37:51 +0200

    (dev) test on latest chrome, firefox and safari
    chores:
    * update saucelab keys
    * update travis badge
    * update travis-ci config
    * enable saucelabs tests
    * run tests on google-chrome-unstable
    * reformat demo files
    * pass lint
    * update pkgs
    
    tests:
    * run tests in correct timezone
    * run tests with the browser default shady or shadow DOM
    * fix saveAs tests in Safari (sinon stub global bug)
    * fix tests with date format that is unknown to Safari 11

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -12,17 +12,5 @@
 		return omnitable;
 	};
 
-	// Detect dom type: dom=shadow vs wc-shadydom=true
-	const getDomType = () => {
-		// match GET parameter named 'dom'
-		let param = window.location.search.match(/(\?|&)dom\=([^&]*)/), // eslint-disable-line no-useless-escape
-			dom;
-		if (Array.isArray(param) && param.length > 0) {
-			dom = decodeURIComponent(param[2]);
-		}
-		const domType = dom && dom === 'shadow' ? ' | Using shadow DOM' : ' | Using shady DOM';
-		return domType;
-	};
-
-	Object.assign(window, {setupOmnitableFixture, getDomType});
+	Object.assign(window, { setupOmnitableFixture });
 }());
