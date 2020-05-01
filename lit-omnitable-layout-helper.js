bfe03428bff82bbee4a66a879a1562abe2ab53f3
Cristian Necula <virusz@gmail.com>
2020-05-01 14:54:03 +0300

    wip

--- lit-omnitable-layout-helper.js
+++ lit-omnitable-layout-helper.js
@@ -1,28 +1,150 @@
-/* eslint-disable one-var */
 import {
-	component, useRef
-} from 'haunted';
-import {
-	directive, html
-} from 'lit-html';
+	component,
+	useRef,
+	useReducer,
+	useLayoutEffect,
+	useCallback,
+	useEffect,
+	useState,
+} from "haunted";
+import { directive, html } from "lit-html";
 
 // eslint-disable-next-line no-return-assign
-const ref = directive(ref => part => ref.current = part.committer.element);
+const ref = directive((ref) => (part) =>
+	(ref.current = part.committer.element)
+);
 
-const
-	hiddenColumns = [],
-	cell = item => ([column]) =>
-		html`<div class="cell" data-column="${column}">${ item[column] }</div>`,
+const intialState = {
+		dimensions: {},
+		hiddenColumns: [],
+	},
+	reducer = (state, action) => {
+		switch (action.type) {
+			case "reset":
+				return intialState;
+			case "update":
+				console.log("update");
+				const hiddenColumns = state.hiddenColumns.filter(
+					(column) => column.breakpoint >= action.currentWidth
+				);
+				return {
+					...state,
+					hiddenColumns,
+					dimensions: {
+						...state.dimensions,
+						...action.dimensions,
+						...Object.fromEntries(
+							hiddenColumns.map((column) => [column.name, 0])
+						),
+					},
+				};
+			case "hideColumn":
+				console.log("hideColumn", action.column);
 
-	layoutRow = (config, hiddenColumns) => item =>
+				return {
+					...state,
+					hiddenColumns: [...state.hiddenColumns, action.column],
+				};
+		}
+	},
+	reset = () => ({ type: "reset" }),
+	update = (currentWidth, dimensions) => ({
+		type: "update",
+		currentWidth,
+		dimensions,
+	}),
+	hideColumn = (breakpoint, name) => ({
+		type: "hideColumn",
+		column: {
+			name,
+			breakpoint,
+		},
+	});
+
+const	cell = (item, column, config, hide) =>
+		html`<div class="cell" data-column="${column}" style="${ hide ? 'display: none' : '' }">${item[column]}</div>`,
+	layoutRow = (config, hiddenColumns, item) =>
 		Object.entries(config)
-			.filter(([column]) => !hiddenColumns.includes(column))
-			.map(cell(item)),
-
-	LayoutHelper = function ({ config, data }) {
+			.map(([column, config]) => cell(item, column, config, hiddenColumns.includes(column))),
+	LayoutHelper = function ({ config, item }) {
 		const layouter = useRef();
+		const [state, dispatch] = useReducer(reducer, intialState);
+		const { dimensions, hiddenColumns } = state;
+		console.log({hiddenColumns})
+		const dispatchEvent = useCallback((event) => this.dispatchEvent(event), []);
+
+		useEffect(() => dispatch(reset()), [dispatch, config]);
+
+		useEffect(() => {
+			let rowWidth;
+			const containerObserver = new ResizeObserver(
+				([row]) => (rowWidth = row.contentRect.width)
+			);
+			const cellObserver = new ResizeObserver((entries) => {
+				console.log('entries', Object.fromEntries(
+					entries
+						.map((entry) => [
+							entry.target.dataset.column,
+							entry.contentRect.width,
+						])
+				))
+				const sizes = Object.fromEntries(
+					entries
+						.filter((entry) => entry.contentRect.width > 0)
+						.map((entry) => [
+							entry.target.dataset.column,
+							entry.contentRect.width,
+						])
+				);
+
+				if(Object.keys(sizes).length === 0) {
+					return;
+				}
+
+				const tooSmall = Object.entries(sizes).find(([column, width]) => {
+					if (width === 0) {
+						return false;
+					}
+
+					const dropThreshold =
+						(config[column] && config[column].dropThreshold) || 100;
+					return width < dropThreshold;
+				});
+
+				if (tooSmall) {
+					dispatch(hideColumn(rowWidth, tooSmall[0]));
+					return;
+				}
+
+				dispatch(update(rowWidth, sizes));
+			});
+
+			containerObserver.observe(layouter.current);
+			layouter.current
+				.querySelectorAll(".cell")
+				.forEach(cellObserver.observe.bind(cellObserver));
+
+			return () => {
+				containerObserver.disconnect();
+				cellObserver.disconnect();
+			};
+		}, [layouter.current, config, dispatch]);
+
+		useEffect(() => {
+			if (Object.keys(dimensions).length === 0) {
+				return;
+			}
+			dispatchEvent(new CustomEvent("layout", { detail: dimensions }));
+		}, [dispatchEvent, dimensions]);
+
 		return html`
 			<style>
+				:host {
+					display: block;
+					/* height: 0; */
+					/* overflow: hidden; */
+				}
+
 				.row {
 					display: flex;
 					background: red;
@@ -37,10 +159,10 @@ const
 					flex-basis: 300px;
 				}
 			</style>
-			<div class="row" ref="${ ref(layouter)}">${
-				layoutRow(config, hiddenColumns)(data)
-			}</div>
+			<div class="row" ref="${ref(layouter)}">
+				${layoutRow(config, hiddenColumns.map(c => c.name), item)}
+			</div>
 		`;
 	};
 
-customElements.define('lit-omnitable-layout-helper', component(LayoutHelper));
+customElements.define("lit-omnitable-layout-helper", component(LayoutHelper));
--- lit-omnitable.js
+++ lit-omnitable.js
@@ -1,73 +1,53 @@
-/* eslint-disable max-statements */
-/* eslint-disable one-var */
 import {
-	component, html, useState, useCallback, useRef, useLayoutEffect, useReducer, useEffect, useMemo
-} from 'haunted';
+	component,
+	html,
+	useState,
+	useCallback,
+	useRef,
+	useLayoutEffect,
+	useReducer,
+	useEffect,
+	useMemo,
+} from "haunted";
 import {
-	useGroupFilterSort, SORT_DESCENDING, SORT_ASCENDING
-} from './lib/use-group-filter-sort';
-import { scroll } from 'lit-virtualizer';
-import { style } from './lit-omnitable.style.js';
-import './lit-omnitable-layout-helper.js'
+	useGroupFilterSort,
+	SORT_DESCENDING,
+	SORT_ASCENDING,
+} from "./lib/use-group-filter-sort";
+import { scroll } from "lit-virtualizer";
+import { style } from "./lit-omnitable.style.js";
+import "./lit-omnitable-layout-helper.js";
+import { nothing } from "lit-html";
 
-const
-	reducer = (state, action) => {
-		switch (action.type) {
-		case 'reset':
-			return {};
-		case 'update':
-			return {
-				...state,
-				...Object.fromEntries(action.entries.map(entry => [entry.target.dataset.column, entry.contentRect.width]))
-			};
-		}
-	},
-	reset = () => ({ type: 'reset' }),
-	update = entries => ({
-		type: 'update',
-		entries
-	});
-
-const
-	cellStyle = width => width > 0 ? `width: ${width }px;` : 'display: none;',
-	layoutStyle = (sizes, hiddenColumns) => Object.entries(sizes).map(([column, width]) => `
-		.header > .cell[data-column="${ column }"],
-		.content > .row > .cell[data-column="${ column }"] {
-			${cellStyle(hiddenColumns.includes(column) ? 0 : width)}
+const cellStyle = (width) =>
+		width > 0 ? `width: ${width}px;` : "display: none;",
+	layoutStyle = (layout = {}) =>
+		Object.entries(layout).map(([column, width]) => `
+		.header > .cell[data-column="${column}"],
+		.content > .row > .cell[data-column="${column}"] {
+			${cellStyle(width)}
 		}
 	`),
-
-	headerCell = (config, onClick) => column =>
-		html`<div class="cell"
-			@click=${onClick}
-			data-column="${column}"
-		>${	config[column].label }</div>`,
-
-	cell = item => ([column, config]) =>
-		html`<div class="cell" data-column="${column}">${
-			item[column]
-		}</div>`,
-
+	headerCell = (config, onClick) => (column) =>
+		html`<div class="cell" @click=${onClick} data-column="${column}">
+			${config[column].label}
+		</div>`,
+	cell = (item) => ([column, config]) =>
+		html`<div class="cell" data-column="${column}">${item[column]}</div>`,
 	// eslint-disable-next-line object-curly-newline
-	row = (item, config) => html`<div class="row">${
-		Object.entries(config).map(cell(item))
-	}</div>`,
-
+	row = (item, config) =>
+		html`<div class="row">${Object.entries(config).map(cell(item))}</div>`,
 	// eslint-disable-next-line max-lines-per-function
-	Omnitable = function ({
-		data, config
-	}) {
+	Omnitable = function ({ data, config }) {
 		const [sortBy, setSortBy] = useState(undefined);
 		const [sortDirection, setSortDirection] = useState(SORT_ASCENDING);
 		const layouter = useRef(null);
 
-		const {
-			visibleItems, configError
-		} = useGroupFilterSort({
+		const { visibleItems, configError } = useGroupFilterSort({
 			config,
 			items: data,
 			sortBy,
-			sortDirection
+			sortDirection,
 		});
 
 		if (configError) {
@@ -75,73 +55,61 @@ const
 		}
 
 		const columns = Object.keys(config);
-		const sortByColumn = useCallback(event => {
-			const newSortBy = event.target.dataset.column;
+		const sortByColumn = useCallback(
+			(event) => {
+				const newSortBy = event.target.dataset.column;
 
-			if (sortBy === newSortBy) {
-				setSortDirection(sortDirection === SORT_ASCENDING ? SORT_DESCENDING : SORT_ASCENDING);
-				return;
-			}
-
-			setSortBy(newSortBy);
-			setSortDirection(SORT_ASCENDING);
-		}, [sortBy, sortDirection, setSortBy, setSortDirection]);
-
-
-		const [sizes, dispatch] = useReducer(reducer, {});
-
-		// useLayoutEffect(() => {
-		// 	dispatch(reset());
-
-		// 	const ro = new ResizeObserver(entries => dispatch(update(entries)));
-		// 	layouter.current.querySelectorAll('.cell').forEach(ro.observe.bind(ro));
-
-		// 	return ro.disconnect.bind(ro);
-		// }, [layouter.current, config, dispatch]);
-
-		const [hiddenColumns, setHiddenColumns] = useState([]);
-		useLayoutEffect(() => {
-			const tooSmall = Object.entries(sizes).find(([column, width]) => {
-				if (hiddenColumns.includes(column)) {
-					return false;
+				if (sortBy === newSortBy) {
+					setSortDirection(
+						sortDirection === SORT_ASCENDING ? SORT_DESCENDING : SORT_ASCENDING
+					);
+					return;
 				}
 
-				const dropThreshold = config[column].dropThreshold || 100;
-				return width < dropThreshold;
-			});
-
-			if (!tooSmall) {
-				return;
-			}
-
-			setHiddenColumns([...hiddenColumns, tooSmall[0]]);
-		}, [sizes, config, setHiddenColumns]);
-
-		useLayoutEffect(() => console.log(hiddenColumns), [hiddenColumns]);
-
-		const lStyle = useMemo(() => layoutStyle(sizes, hiddenColumns), [sizes]);
+				setSortBy(newSortBy);
+				setSortDirection(SORT_ASCENDING);
+			},
+			[sortBy, sortDirection, setSortBy, setSortDirection]
+		);
 
 		// BUG: lit-virtualizer does not support changing renderItem at runtime (https://github.com/PolymerLabs/uni-virtualizer/issues/34)
 		// this means that config changes will be ignored
-		const renderItem = useCallback(item => row(item, config), [config]);
+		const renderItem = useCallback((item) => row(item, config), [config]);
+
+		const [layout, setLayout] = useState(undefined);
+		const onLayout = useCallback((event) => setLayout(event.detail), []);
+		const lStyle = useMemo(() => layoutStyle(layout), [layout]);
+		useMemo(() => console.log(layout), [layout]);
 
 		/* eslint-disable indent */
 		return html`
 			<style>
-				${ style }
-				${ lStyle }
+				${style}
+				${lStyle}
 			</style>
-			<div>SortBy: ${ sortBy }</div>
-			<div>SortDirection: ${ sortDirection }</div>
-			<div class="header">${ columns.map(headerCell(config, sortByColumn)) }</div>
+			<div>SortBy: ${sortBy}</div>
+			<div>SortDirection: ${sortDirection}</div>
+			<div class="header">${
+				layout == null
+					? nothing
+					:	columns.map(headerCell(config, sortByColumn))
+			}</div>
 			<div class="content">
-				<lit-omnitable-layout-helper .config="${ config }" .data="${ visibleItems[0] }"></lit-omnitable-layout-helper>
-				${scroll({
-					items: visibleItems,
-					renderItem
-				})}
+				<lit-omnitable-layout-helper
+					.config="${config}"
+					.item="${visibleItems[0]}"
+					@layout="${onLayout}"
+				></lit-omnitable-layout-helper>
+				${
+					layout == null
+						? nothing
+						: scroll({
+							items: visibleItems,
+							renderItem,
+						})
+					}
 			</div>
 		`;
 	};
 
-customElements.define('lit-omnitable', component(Omnitable));
+customElements.define("lit-omnitable", component(Omnitable));
