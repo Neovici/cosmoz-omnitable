b6741e2001cdc2f223c7b394507fea0c12186615
Meghea Iulian <iulian.meghea@gmail.com>
2020-06-03 18:04:26 +0300

    chore(deps): upgrade

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 # see https://nixos.wiki/wiki/Development_environment_with_nix-shell
 {}:
-with import (
-  builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/fce7562.tar.gz";
-    sha256 = "14rvi69ji61x3z88vbn17rg5vxrnw2wbnanxb7y0qzyqrj7spapx";
-  }
-) {};
+with import
+  (
+    builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/467ce5a.tar.gz";
+      sha256 = "0qz7wgi61pdb335n18xm8rfwddckwv0vg8n7fii5abrrx47vnqcj";
+    }
+  ) { };
 mkShell {
   buildInputs = [ nodejs-12_x python3 firefox ];
   shellHook = ''
