Neovici/cosmoz-omnitable

commit 3254986971a45d696e55ec1e9c4bcba7f05b3c90

fix: replace iron-resizable-behavior with a ResizeObserver

cosmoz-omnitable.js
@@ -37,12 +37,9 @@ import {
 } from '@polymer/polymer/lib/utils/async';
 import { Debouncer } from '@polymer/polymer/lib/utils/debounce';
 import { FlattenedNodesObserver } from '@polymer/polymer/lib/utils/flattened-nodes-observer';
-
 import { PolymerElement } from '@polymer/polymer/polymer-element';
 import { html } from '@polymer/polymer/lib/utils/html-tag';
 
-import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class';
-import { IronResizableBehavior } from '@polymer/iron-resizable-behavior/iron-resizable-behavior';
 import { translatable } from '@neovici/cosmoz-i18next';
 import { mixin } from '@neovici/cosmoz-utils';
 import { isEmpty } from '@neovici/cosmoz-utils/lib/template.js';
@@ -58,13 +55,7 @@ const PROPERTY_HASH_PARAMS = ['sortOn', 'groupOn', 'descending', 'groupOnDescend
  * @demo demo/index.html
  */
 
-class Omnitable extends mixin({
-	isEmpty
-}, translatable(
-	mixinBehaviors([
-		IronResizableBehavior
-	], PolymerElement)
-)) {
+class Omnitable extends mixin({ isEmpty }, translatable(PolymerElement)) {
 	/* eslint-disable-next-line max-lines-per-function */
 	static get template() {
 		const template = html`
@@ -481,6 +472,7 @@ class Omnitable extends mixin({
 		this._filterItems = this._filterItems.bind(this);
 		this._groupItems = this._groupItems.bind(this);
 		this._sortFilteredGroupedItems = this._sortFilteredGroupedItems.bind(this);
+		this._resizeObserver = new ResizeObserver(this._onResize.bind(this));
 	}
 
 	connectedCallback() {
@@ -506,13 +498,13 @@ class Omnitable extends mixin({
 		this.addEventListener('cosmoz-column-hidden-changed', this._debounceUpdateColumns);
 		this.addEventListener('cosmoz-column-disabled-changed', this._debounceUpdateColumns);
 
-		this.addEventListener('iron-resize', this._onResize);
 		this.addEventListener('update-item-size', this._onUpdateItemSize);
 		this.addEventListener('cosmoz-column-title-changed', this._onColumnTitleChanged);
 		this.addEventListener('cosmoz-column-filter-changed', this._filterChanged);
 		this.addEventListener('cosmoz-column-editable-changed', this._onColumnEditableChanged);
 		this.addEventListener('cosmoz-column-values-update', this._onColumnValuesUpdate);
 		this._fitDropdowns();
+		this._resizeObserver.observe(this);
 	}
 
 	disconnectedCallback() {
@@ -527,12 +519,12 @@ class Omnitable extends mixin({
 		// Just in case we get detached before a planned debouncer has not run yet.
 		this._cancelDebouncers();
 
-		this.removeEventListener('iron-resize', this._onResize);
 		this.removeEventListener('update-item-size', this._onUpdateItemSize);
 		this.removeEventListener('cosmoz-column-title-changed', this._onColumnTitleChanged);
 		this.removeEventListener('cosmoz-column-filter-changed', this._filterChanged);
 		this.removeEventListener('cosmoz-column-editable-changed', this._onColumnEditableChanged);
 		this.removeEventListener('cosmoz-column-values-update', this._onColumnValuesUpdate);
+		this._resizeObserver.unobserve(this);
 	}
 
 	flush() {
@@ -665,8 +657,12 @@ class Omnitable extends mixin({
 		this.highlight(item, this.isItemHighlighted(item));
 	}
 
-	_onResize() {
-		this._setVisible(this.offsetParent != null);
+	_onResize([entry]) {
+		const hidden = entry.borderBoxSize?.[0]?.blockSize === 0 || entry.contentRect?.height === 0;
+		this._setVisible(!hidden);
+		if (hidden) {
+			return;
+		}
 		this._debounceAdjustColumns();
 	}
 
@@ -744,7 +740,7 @@ class Omnitable extends mixin({
 			return;
 		}
 
-		let columns = this.getEffectiveChildren().filter((child, index) => {
+		let columns = Array.from(this.children).filter((child, index) => {
 				child.__index = index;
 				// filter only omnitable columns
 				return child.nodeType === Node.ELEMENT_NODE && child.isOmnitableColumn
@@ -1475,7 +1471,7 @@ class Omnitable extends mixin({
 	}
 
 	_routeHashPropertyChanged(key, value) {
-		const deserialized = this.deserialize(value, Omnitable.properties[key].type);
+		const deserialized = this._deserializeValue(value, Omnitable.properties[key].type);
 		if (deserialized === this.get(key)) {
 			return;
 		}
@@ -1546,7 +1542,7 @@ class Omnitable extends mixin({
 		const path = ['_routeHash', this.hashParam + '-' + key],
 			hashValue = this.get(path),
 			value = this.get(key),
-			serialized = this.serialize(value, Omnitable.properties[key].type);
+			serialized = this._serializeValue(value, Omnitable.properties[key].type);
 
 		if (serialized === hashValue) {
 			return;

shell.nix
@@ -3,14 +3,22 @@
 with import
   (
     builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/467ce5a.tar.gz";
-      sha256 = "0qz7wgi61pdb335n18xm8rfwddckwv0vg8n7fii5abrrx47vnqcj";
+      url = "https://github.com/NixOS/nixpkgs/archive/daaa0e33505082716beb52efefe3064f0332b521.tar.gz";
+      sha256 = "15vprzpbllp9hy5md36ch1llzhxhd44d291kawcslgrzibw51f95";
     }
-  ) { };
+  )
+{ };
 mkShell {
-  buildInputs = [ nodejs-12_x python3 firefox ];
+  buildInputs = [ nodejs-12_x python3 firefox jre azure-storage-azcopy google-chrome ];
   shellHook = ''
     export CHROME_BIN=${google-chrome}/bin/google-chrome-stable
     export PATH=$(npm bin):$PATH
+
+    link_selenium () {
+      if [ -d "./node_modules" ]; then
+        ln -sfnv "${chromedriver}/bin/chromedriver" "$(find "./node_modules" -path '**/chromedriver/*-chromedriver')"
+        ln -sfnv "${geckodriver}/bin/geckodriver" "$(find "./node_modules" -path '**/geckodriver/*-geckodriver')"
+      fi
+    }
   '';
 }

commit e0c7b6e19cd42449ee4171efe9e76e921c6dc3ee

fix(update-columns): slotted columns are not displayed
Broken in 3254986971a45d696e55ec1e9c4bcba7f05b3c90

cosmoz-omnitable.js
@@ -42,6 +42,7 @@ import { html as litHtml } from 'lit-html';
 import { translatable } from '@neovici/cosmoz-i18next';
 import { mixin } from '@neovici/cosmoz-utils';
 import { isEmpty } from '@neovici/cosmoz-utils/lib/template.js';
+import { getEffectiveChildrenLegacyMixin } from './get-effective-children-legacy-mixin';
 
 const PROPERTY_HASH_PARAMS = ['sortOn', 'groupOn', 'descending', 'groupOnDescending'];
 
@@ -54,7 +55,7 @@ const PROPERTY_HASH_PARAMS = ['sortOn', 'groupOn', 'descending', 'groupOnDescend
  * @demo demo/index.html
  */
 
-class Omnitable extends mixin({ isEmpty }, translatable(PolymerElement)) {
+class Omnitable extends mixin({ isEmpty }, getEffectiveChildrenLegacyMixin(translatable(PolymerElement))) {
 	/* eslint-disable-next-line max-lines-per-function */
 	static get template() {
 		const template = html`
@@ -730,7 +731,8 @@ class Omnitable extends mixin({ isEmpty }, translatable(PolymerElement)) {
 			return;
 		}
 
-		let columns = Array.from(this.children).filter((child, index) => {
+		// NOTE: it's important to get all children, including those projected in slots
+		let columns = this.getEffectiveChildren().filter((child, index) => {
 				child.__index = index;
 				// filter only omnitable columns
 				return child.nodeType === Node.ELEMENT_NODE && child.isOmnitableColumn

get-effective-children-legacy-mixin.js
@@ -1,0 +1,37 @@
+
+import { dom } from '@polymer/polymer/lib/legacy/polymer.dom';
+
+export const getEffectiveChildrenLegacyMixin = base => class extends base {
+	/**
+	 * Returns a list of nodes that are the effective childNodes. The effective
+	 * childNodes list is the same as the element's childNodes except that
+	 * any `<content>` elements are replaced with the list of nodes distributed
+	 * to the `<content>`, the result of its `getDistributedNodes` method.
+	 * @return {!Array<!Node>} List of effective child nodes.
+	 * @suppress {invalidCasts} LegacyElementMixin must be applied to an
+	 *     HTMLElement
+	 * @override
+	 */
+	getEffectiveChildNodes() {
+		// eslint-disable-next-line consistent-this
+		const thisEl = /** @type {Element} */ this,
+			domApi = /** @type {PolymerDomApi} */ dom(thisEl);
+		return domApi.getEffectiveChildNodes();
+	}
+
+	/**
+	 * Returns a list of elements that are the effective children. The effective
+	 * children list is the same as the element's children except that
+	 * any `<content>` elements are replaced with the list of elements
+	 * distributed to the `<content>`.
+	 *
+	 * @return {!Array<!Node>} List of effective children.
+	 * @override
+	 */
+	getEffectiveChildren() {
+		const list = this.getEffectiveChildNodes();
+		return list.filter((/** @type {!Node} */ n) => {
+			return n.nodeType === Node.ELEMENT_NODE;
+		});
+	}
+};