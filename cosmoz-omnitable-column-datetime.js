8b1ccb009dd183633df5ed46d0c50a83eec25209
Cristian Necula <virusz@gmail.com>
2021-03-19 15:07:06 +0200

    fix(column-datetime): migrate templates to render functions
    Re https://github.com/Neovici/cosmoz-frontend/issues/3282

--- cosmoz-omnitable-column-datetime.js
+++ cosmoz-omnitable-column-datetime.js
@@ -1,12 +1,16 @@
+/* eslint-disable no-return-assign */
 import '@polymer/paper-dropdown-menu/paper-dropdown-menu';
 import '@neovici/cosmoz-datetime-input';
 import './ui-helpers/cosmoz-clear-button';
 
 import { dateColumnMixin } from './cosmoz-omnitable-column-date-mixin';
 import { columnMixin } from './cosmoz-omnitable-column-mixin';
-import { translatable } from '@neovici/cosmoz-i18next';
+import {
+	translatable, _
+} from '@neovici/cosmoz-i18next';
 import { PolymerElement } from '@polymer/polymer/polymer-element';
-import { html } from '@polymer/polymer/lib/utils/html-tag';
+import { html } from 'lit-html';
+import { ifDefined } from 'lit-html/directives/if-defined';
 window.Cosmoz = window.Cosmoz || {};
 
 /**
@@ -21,36 +25,6 @@ class OmnitableColumnDatetime extends
 			translatable(PolymerElement)
 		)
 	) {
-	static get template() {
-		return html`
-		<template class="cell" strip-whitespace>
-			[[ getString(item, valuePath, formatter) ]]
-		</template>
-
-		<template class="edit-cell" strip-whitespace>
-			<cosmoz-datetime-input no-label-float type="cosmoz-datetime-input" on-change="_dateValueChanged" value="[[ getInputString(item, valuePath) ]]">
-			</cosmoz-datetime-input>
-		</template>
-
-		<template class="header" strip-whitespace>
-			<cosmoz-clear-button on-click="resetFilter" visible="[[ hasFilter(filter.*) ]]"></cosmoz-clear-button>
-			<paper-dropdown-menu label="[[ title ]]" placeholder="[[ _filterText ]]"
-				class$="external-values-[[ externalValues ]]"
-				title="[[ _tooltip ]]" horizontal-align="[[ preferredDropdownHorizontalAlign ]]" opened="{{ headerFocused }}">
-				<div class="dropdown-content" slot="dropdown-content" style="padding: 15px; min-width: 100px;">
-					<h3 style="margin: 0;">[[ title ]]</h3>
-					<cosmoz-datetime-input date-label="[[ _('From date', t) ]]" time-label="[[ _('From time', t) ]]"
-						min="[[ _toInputString(_limit.fromMin) ]]" max="[[ _toInputString(_limit.fromMax) ]]" value="{{ _filterInput.min }}" step$="[[ filterStep ]]">
-					</cosmoz-datetime-input>
-					<cosmoz-datetime-input date-label="[[ _('To date', t) ]]" time-label="[[ _('To time', t) ]]"
-						min="[[ _toInputString(_limit.toMin) ]]" max="[[ _toInputString(_limit.toMax) ]]" value="{{ _filterInput.max }}" step$="[[ filterStep ]]">
-					</cosmoz-datetime-input>
-				</div>
-			</paper-dropdown-menu>
-		</template>
-`;
-	}
-
 	static get is() {
 		return 'cosmoz-omnitable-column-datetime';
 	}
@@ -87,6 +61,59 @@ class OmnitableColumnDatetime extends
 				value: 'datetime-header-cell'
 			}
 		};
+	}
+
+	renderCell(column, { item }) {
+		return column.getString(item, column.valuePath, column.formatter);
+	}
+
+	renderEditCell(column, { item }) {
+		const onChange = event => {
+			event.model = { item };
+			return column._dateValueChanged(event);
+		};
+
+		return html`<cosmoz-datetime-input
+			no-label-float
+			@value-changed=${ onChange }
+			.value=${ column.getInputString(item, column.valuePath) }
+		></cosmoz-datetime-input>`;
+	}
+
+	renderHeader(column) {
+		return html`
+			<cosmoz-clear-button @click=${ event => column.resetFilter(event) } ?visible=${ column.hasFilter() }></cosmoz-clear-button>
+			<paper-dropdown-menu
+				label=${ column.title }
+				placeholder=${ ifDefined(column._filterText) }
+				class="external-values-${ column.externalValues }"
+				title=${ column._tooltip }
+				horizontal-align=${ column.preferredDropdownHorizontalAlign }
+				?opened=${ column.headerFocused }
+				@opened-changed=${ event => column.headerFocused = event.detail.value }>
+				<div class="dropdown-content" slot="dropdown-content" style="padding: 15px; min-width: 100px;">
+					<h3 style="margin: 0;">${ column.title }</h3>
+					<cosmoz-datetime-input
+						date-label=${ _('From date') }
+						time-label=${ _('From time') }
+						min=${ column._toInputString(column._limit.fromMin) }
+						max=${ column._toInputString(column._limit.fromMax) }
+						step=${ column.filterStep }
+						.value=${ column._filterInput.min }
+						@value-changed=${ event => column.set('_filterInput.min', event.detail.value) }
+					></cosmoz-datetime-input>
+					<cosmoz-datetime-input
+						date-label=${ _('To date') }
+						time-label=${ _('To time') }
+						min=${ column._toInputString(column._limit.toMin) }
+						max=${ column._toInputString(column._limit.toMax) }
+						step=${ column.filterStep }
+						.value=${ column._filterInput.max }
+						@value-changed=${ event => column.set('_filterInput.max', event.detail.value) }
+					></cosmoz-datetime-input>
+				</div>
+			</paper-dropdown-menu>
+		`;
 	}
 
 	_toInputString(value) {
