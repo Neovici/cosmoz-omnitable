39642ef3f0d92a13dc0b0c05695105a33028becd
Luta Vasile <elsile69@yahoo.com>
2022-02-23 15:12:37 +0200

    test: new unit tests utils-amount
    Add @neovici/cfg: prettier, eslint rules and test runner

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,33 +1,3 @@
-import { chromeLauncher } from '@web/test-runner';
-import { seleniumLauncher } from '@web/test-runner-selenium';
-import webdriver from 'selenium-webdriver';
-import firefox from 'selenium-webdriver/firefox.js';
+import cfg from '@neovici/cfg/web/test-runner.mjs';
 
-export default {
-	nodeResolve: true,
-	coverageConfig: {
-		reportDir: 'coverage',
-		exclude: ['test/**/*.js', 'demo/**/*.js', 'ui-helpers/*.js', 'node_modules/**'],
-		threshold: {
-			statements: 70,
-			branches: 60,
-			functions: 79,
-			lines: 70
-		}
-	},
-	testFramework: {
-		config: {
-			ui: 'tdd'
-		}
-	},
-	files: [
-		'test/**/*test.js'
-	],
-	browsers: [
-		chromeLauncher(),
-		seleniumLauncher({
-			driverBuilder: new webdriver.Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless())
-		})
-	]
-
-};
+export default cfg;
