c5d8111b2607a15f1266ec6fa9ed9cc3530ad60b
Meghea Iulian <iulian.meghea@gmail.com>
2022-04-28 16:45:53 +0300

    fix(cosmoz-omnitable): clear sort & group
    Fixes clear button for sort/group not resetting sortOn/groupOn.
    Cleans up header-row
    Updates & cleans up some deps

--- cosmoz-omnitable-header-row.js
+++ cosmoz-omnitable-header-row.js
@@ -1,31 +1,47 @@
-/* eslint-disable object-curly-newline */
 import { html, component } from 'haunted';
 import { repeat } from 'lit-html/directives/repeat';
 import './lib/cosmoz-omnitable-resize-nub';
 
-const
-	renderHeaderRow = ({ data, columns, groupOnColumn, filters, setFilterState }) => {
-		return repeat(columns, column => column.name, column => [
-			html`<div
-				class="cell ${ column.headerCellClass } header-cell"
-				?hidden=${ column === groupOnColumn }
-				title=${ column.title }
-				name=${ column.name }
-			>${ column.renderHeader(
-		column,
-		filters[column.name] ?? {},
-		state => setFilterState(column.name, state),
-		column.source(column, data)
-	) }</div>`,
-			html`<cosmoz-omnitable-resize-nub
-				.column=${ column }
-				name=${ column.name }
-			></cosmoz-omnitable-resize-nub>`
-		]);
-	},
+const renderHeaderRow = ({
+		data,
+		columns,
+		groupOnColumn,
+		filters,
+		setFilterState,
+	}) =>
+		repeat(
+			columns,
+			(column) => column.name,
+			(column) => [
+				html`<div
+					class="cell ${column.headerCellClass} header-cell"
+					?hidden=${column === groupOnColumn}
+					title=${column.title}
+					name=${column.name}
+				>
+					${column.renderHeader(
+						column,
+						filters[column.name] ?? {},
+						(state) => setFilterState(column.name, state),
+						column.source(column, data)
+					)}
+				</div>`,
+				html`<cosmoz-omnitable-resize-nub
+					.column=${column}
+					name=${column.name}
+				></cosmoz-omnitable-resize-nub>`,
+			]
+		),
+	HeaderRow = ({ content, columns, ...thru }) => [
+		columns &&
+			renderHeaderRow({
+				columns,
+				...thru,
+			}),
+		content,
+	];
 
-	HeaderRow = ({ data, columns, groupOnColumn, content, filters, setFilterState }) => {
-		return [columns && renderHeaderRow({ data, columns, groupOnColumn, filters, setFilterState }), content];
-	};
-
-customElements.define('cosmoz-omnitable-header-row', component(HeaderRow, { useShadowDOM: false }));
+customElements.define(
+	'cosmoz-omnitable-header-row',
+	component(HeaderRow, { useShadowDOM: false })
+);
