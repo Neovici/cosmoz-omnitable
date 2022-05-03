027b344075dac1d80bdfa3cf08fed89472619e6b
Meghea Iulian <iulian.meghea@gmail.com>
2022-05-03 07:12:02 +0300

    refactor: separate sort

--- cosmoz-omnitable-header-row.js
+++ cosmoz-omnitable-header-row.js
@@ -1,48 +1,9 @@
 import { html, component } from 'haunted';
 import { repeat } from 'lit-html/directives/repeat';
-import { ifDefined } from 'lit-html/directives/if-defined';
 import './lib/cosmoz-omnitable-resize-nub';
+import sort from './lib/cosmoz-omnitable-sort';
 
-const sortIcon = html`<svg
-		width="8"
-		height="6"
-		viewBox="0 0 8 6"
-		fill="currentColor"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<path
-			d="M3.56699 0.75C3.75944 0.416666 4.24056 0.416667 4.43301 0.75L7.03109 5.25C7.22354 5.58333 6.98298 6 6.59808 6L1.40192 6C1.01702 6 0.776461 5.58333 0.968911 5.25L3.56699 0.75Z"
-		/>
-	</svg> `,
-	sort = ({
-		column,
-		sortOnColumn,
-		setSortOn,
-		descending,
-		setDescending,
-	}) => html` <button
-		class="sort"
-		data-sort=${ifDefined(
-			(column === sortOnColumn && (descending ? 'desc' : 'asc')) || undefined
-		)}
-		@click=${(e) => {
-			const sort = e.currentTarget?.dataset.sort;
-			if (!sort) {
-				setSortOn(column.name);
-				setDescending(false);
-			}
-			if (sort === 'asc') {
-				setDescending(true);
-			}
-			if (sort === 'desc') {
-				setSortOn();
-				setDescending(false);
-			}
-		}}
-	>
-		${sortIcon}
-	</button>`,
-	/* eslint-disable-next-line max-lines-per-function */
+const /* eslint-disable-next-line max-lines-per-function */
 	renderHeaderRow = ({
 		data,
 		columns,
