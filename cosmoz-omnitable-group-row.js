a3de747a3e0a96c6b7b188f865b24ca038c70035
Meghea Iulian <iulian.meghea@gmail.com>
2022-05-04 21:23:59 +0300

    fix(group-row): pass group to render-group

--- cosmoz-omnitable-group-row.js
+++ cosmoz-omnitable-group-row.js
@@ -1,12 +1,12 @@
 import { component } from 'haunted';
 import { nothing } from 'lit-html';
 
-const GroupRow = ({ column, item, selected, folded }) => {
+const GroupRow = ({ column, item, selected, folded, group }) => {
 	if (!column) {
 		return nothing;
 	}
 
-	return (column.renderGroup ?? column.renderCell)(column, { item, selected, folded });
+	return (column.renderGroup ?? column.renderCell)(column, { item, selected, folded, group });
 };
 
 customElements.define('cosmoz-omnitable-group-row', component(GroupRow, { useShadowDOM: false }));
