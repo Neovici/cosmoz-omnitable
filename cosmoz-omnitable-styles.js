6a219b9959a4b94c59e04cf7131364a39de346c3
CarloBar1 <carlo.barone@neovici.se>
2023-02-16 14:35:44 +0100

    fix: change font in omnitable headers, change omnitable settings icon

--- cosmoz-omnitable-styles.js
+++ cosmoz-omnitable-styles.js
@@ -132,7 +132,33 @@ export default css`
 			--paper-font-caption_-_line-height: 18px;
 		}
 	}
+	cosmoz-omnitable-amount-range-input,
+	cosmoz-omnitable-number-range-input,
+	cosmoz-omnitable-date-range-input,
+	cosmoz-omnitable-time-range-input,
+	cosmoz-omnitable-datetime-range-input,
+	paper-input {
+		--paper-font-subhead_-_font-family: var(
+			--cosmoz-omnitable-header-font-family,
+			'Inter' sans-serif
+		);
+		text-transform: var(--cosmoz-omnitable-header-text-transform, uppercase);
+		--paper-font-subhead_-_font-weight: var(
+			--cosmoz-omnitable-header-font-weight,
+			bold
+		);
+		--paper-font-subhead_-_font-size: var(
+			--cosmoz-omnitable-header-font-size,
+			14pt
+		);
+	}
 
+	cosmoz-autocomplete-ui::part(input-label) {
+		text-transform: var(--cosmoz-omnitable-header-text-transform, uppercase);
+		font-weight: var(--cosmoz-omnitable-header-font-weight, bold);
+		font-family: var(--cosmoz-omnitable-header-font-family, 'Inter' sans-serif);
+		font-size: var(--cosmoz-omnitable-header-font-size, 14pt);
+	}
 	cosmoz-omnitable-header-row {
 		white-space: nowrap;
 	}
