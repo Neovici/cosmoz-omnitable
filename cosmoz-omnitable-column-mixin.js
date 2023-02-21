43812d53470871f1b2355a758f909676b17fd2a3
Joakim Carlsson Kekonius <joakim.carlsson.kekonius@neovici.se>
2023-02-21 09:39:24 +0100

    feat(filter): applying trim for more lenient searching

--- cosmoz-omnitable-column-mixin.js
+++ cosmoz-omnitable-column-mixin.js
@@ -1,126 +1,140 @@
 import { get } from '@polymer/polymer/lib/utils/path';
 
-export const
-	getString = ({ valuePath }, item) => get(item, valuePath),
+export const getString = ({ valuePath }, item) => get(item, valuePath),
 	toXlsxValue = getString,
 	getComparableValue = getString,
-
-	applySingleFilter = ({ valuePath }, filter) => item => {
-		const value = get(item, valuePath);
-		if (value == null) {
-			return false;
-		}
-		return value.toString().toLowerCase().includes(filter.toLowerCase());
-	},
-
-	serializeFilter = (column, filter) => filter === '' || filter == null ? null : filter,
-
+	applySingleFilter =
+		({ valuePath }, filter) =>
+		(item) => {
+			const value = get(item, valuePath);
+			if (value == null) {
+				return false;
+			}
+			return value
+				.toString()
+				.toLowerCase()
+				.trim()
+				.includes(filter.toLowerCase().trim());
+		},
+	serializeFilter = (column, filter) =>
+		filter === '' || filter == null ? null : filter,
 	// eslint-disable-next-line max-lines-per-function
-	columnMixin = base => class extends base {
-		static get properties() {
-			return {
+	columnMixin = (base) =>
+		class extends base {
+			static get properties() {
+				return {
+					/**
+					 * Used to indicate that an element using this behavior is a column definition that can be used
+					 * in cosmoz-omnitable
+					 */
+					isOmnitableColumn: { type: Boolean, value: true },
+					title: { type: String },
+					valuePath: { type: String, notify: true },
+					values: { type: Array, notify: true },
+					filter: { type: Object },
+					/**
+					 * If the column should be disabled until enabled with enabledColumns
+					 */
+					disabled: { type: Boolean, value: false, notify: true },
+					/**
+					 * If true, the column will be editable by using an input element for rendering.
+					 */
+					editable: { type: Boolean, notify: true },
+					/**
+					 * Indicate that the column is loading/performing work
+					 */
+					loading: { type: Boolean, value: false, notify: true },
+					externalValues: { type: Boolean, value: false, notify: true },
+					/**
+					 * Column name for use with enabledColumns
+					 */
+					name: { type: String },
+					sortOn: { type: String },
+					groupOn: { type: String },
+					width: { type: String, value: '75px' },
+					minWidth: { type: String, value: '40px' },
+					flex: { type: String, value: '1' },
+					cellClass: { type: String, value: 'default-cell' },
+					headerCellClass: { type: String, value: 'default-header-cell' },
+					priority: { type: Number, value: 0 },
+					hidden: { type: Boolean, notify: true },
+					preferredDropdownHorizontalAlign: { type: String, value: 'right' },
+					renderHeader: { type: Function },
+					renderCell: { type: Function },
+					renderEditCell: { type: Function },
+					renderGroup: { type: Function },
+				};
+			}
+
+			static get observers() {
+				return ['notifyFilterChange(filter)'];
+			}
+
+			notifyFilterChange(filter) {
+				if (this.__ownChange) {
+					return;
+				}
+				this.dispatchEvent(
+					new CustomEvent('legacy-filter-changed', {
+						detail: {
+							name: this.name,
+							state: this.legacyFilterToState(filter),
+						},
+						bubbles: true,
+					})
+				);
+			}
+
+			legacyFilterToState(filter) {
+				return { filter };
+			}
+
 			/**
-			 * Used to indicate that an element using this behavior is a column definition that can be used
-			 * in cosmoz-omnitable
+			 * Override this in column elements if you need a different default width
 			 */
-				isOmnitableColumn: { type: Boolean, value: true },
-				title: { type: String },
-				valuePath: { type: String, notify: true },
-				values: { type: Array, notify: true },
-				filter: { type: Object },
-				/**
-			 * If the column should be disabled until enabled with enabledColumns
-			 */
-				disabled: { type: Boolean, value: false, notify: true },
-				/**
-			 * If true, the column will be editable by using an input element for rendering.
-			 */
-				editable: { type: Boolean, notify: true },
-				/**
-			 * Indicate that the column is loading/performing work
-			 */
-				loading: { type: Boolean, value: false, notify: true },
-				externalValues: { type: Boolean, value: false, notify: true },
-				/**
-			 * Column name for use with enabledColumns
-			 */
-				name: { type: String },
-				sortOn: { type: String },
-				groupOn: { type: String },
-				width: { type: String, value: '75px' },
-				minWidth: { type: String, value: '40px' },
-				flex: { type: String, value: '1' },
-				cellClass: { type: String, value: 'default-cell' },
-				headerCellClass: { type: String, value: 'default-header-cell' },
-				priority: { type: Number, value: 0 },
-				hidden: { type: Boolean, notify: true },
-				preferredDropdownHorizontalAlign: { type: String, value: 'right' },
-				renderHeader: { type: Function },
-				renderCell: { type: Function },
-				renderEditCell: { type: Function },
-				renderGroup: { type: Function }
-			};
-		}
 
-		static get observers() {
-			return ['notifyFilterChange(filter)'];
-		}
+			// eslint-disable-next-line no-empty-function
+			getFilterFn() {}
 
-		notifyFilterChange(filter) {
-			if (this.__ownChange) {
-				return;
-			}
-			this.dispatchEvent(new CustomEvent('legacy-filter-changed', { detail: { name: this.name, state: this.legacyFilterToState(filter) }, bubbles: true }));
-		}
-
-		legacyFilterToState(filter) {
-			return { filter };
-		}
-
-		/**
-	 * Override this in column elements if you need a different default width
-	 */
-
-		// eslint-disable-next-line no-empty-function
-		getFilterFn() {}
-
-		getString(column, item) {
-			return getString(column, item);
-		}
-
-		toXlsxValue(column, item) {
-			return toXlsxValue(column, item);
-		}
-
-		cellTitleFn(column, item) {
-			return getString(column, item);
-		}
-
-		serializeFilter(column, filter) {
-			return serializeFilter(column, filter);
-		}
-
-		deserializeFilter(column, filter) {
-			if (filter == null) {
-				return null;
+			getString(column, item) {
+				return getString(column, item);
 			}
 
-			if (typeof filter === 'string') {
-				return window.decodeURIComponent(filter);
+			toXlsxValue(column, item) {
+				return toXlsxValue(column, item);
 			}
-			return filter;
-		}
 
-		getComparableValue(column, item) {
-			return getComparableValue(column, item);
-		}
+			cellTitleFn(column, item) {
+				return getString(column, item);
+			}
 
-		computeSource(column, data) {
-			return data;
-		}
+			serializeFilter(column, filter) {
+				return serializeFilter(column, filter);
+			}
 
-		_propertiesChanged(currentProps, changedProps, oldProps) {
-			super._propertiesChanged(currentProps, changedProps, oldProps);
-			this.dispatchEvent(new CustomEvent('cosmoz-column-prop-changed', { bubbles: true }));
-		}
-	};
+			deserializeFilter(column, filter) {
+				if (filter == null) {
+					return null;
+				}
+
+				if (typeof filter === 'string') {
+					return window.decodeURIComponent(filter);
+				}
+				return filter;
+			}
+
+			getComparableValue(column, item) {
+				return getComparableValue(column, item);
+			}
+
+			computeSource(column, data) {
+				return data;
+			}
+
+			_propertiesChanged(currentProps, changedProps, oldProps) {
+				super._propertiesChanged(currentProps, changedProps, oldProps);
+				this.dispatchEvent(
+					new CustomEvent('cosmoz-column-prop-changed', { bubbles: true })
+				);
+			}
+		};
