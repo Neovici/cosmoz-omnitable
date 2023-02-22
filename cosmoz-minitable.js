80c7ad6e5edf181f10e6508cb88aaf6bfd1f0533
Carlo Barone <carlo.barone@neovici.se>
2023-02-22 13:37:00 +0100

    refactor: wip

---
+++ cosmoz-minitable.js
@@ -0,0 +1,9 @@
+import { component, html } from 'haunted';
+
+const Minitable = () => {
+    return html`<cosmoz-tab-card>
+
+    </cosmoz-tab-card>`;
+}
+
+customElements.define('cosmoz-minitable', component(Minitable));
