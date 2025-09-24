422d746c4fcf4dcf16ab560beef632ee7bc5c8c8
John <codingjohn@outlook.com>
2025-09-24 23:52:33 +0800

    chore: start migration

---
+++ src/lib/utils-amount.ts
@@ -0,0 +1,203 @@
+import { get } from '@polymer/polymer/lib/utils/path';
+import { toNumber } from './utils-number';
+
+type Currency = 'SEK' | 'USD' | 'EUR' | 'AUD';
+
+interface AmountValue {
+	amount: string | number;
+	currency: Currency;
+}
+
+type Rates = Record<Currency, number> | {};
+
+type Limit = number | null;
+
+type LimitFunc = (a: number, b: number) => number | undefined;
+
+interface Column {
+	valuePath?: string;
+	rates?: Rates;
+	locale?: string;
+}
+
+interface FilterColumn extends Column {
+	valuePath: string;
+}
+
+interface FilterValue extends Record<string, unknown> {
+	min?: AmountValue | null;
+	max?: AmountValue | null;
+}
+
+type Item = Record<string, unknown>;
+
+const isValidAmountValue = (value: unknown): value is AmountValue => {
+	return (
+		typeof value === 'object' &&
+		value != null &&
+		'currency' in value &&
+		'amount' in value &&
+		(value as AmountValue).currency != null
+	);
+};
+
+const convertToAmount = (value: AmountValue): AmountValue | null => {
+	const number = toNumber(value.amount);
+	if (number == null || Number.isNaN(number)) {
+		return null;
+	}
+	return { currency: value.currency, amount: number };
+};
+
+/**
+ * Converts a value to an amount object optionally limiting it.
+ */
+export const toAmount = (
+	rates: Rates = {},
+	value: AmountValue | number | null | '',
+	limit?: Limit | null,
+	limitFunc?: LimitFunc,
+): AmountValue | null => {
+	console.log('toAmount', { rates, value, limit, limitFunc });
+	if (value == null || value === '' || !isValidAmountValue(value)) {
+		return null;
+	}
+
+	const amount = convertToAmount(value);
+	if (amount == null || limitFunc == null || limit == null) {
+		return amount;
+	}
+
+	const lAmount = toAmount(rates, limit);
+	if (lAmount == null) return amount;
+
+	const valAmount = (amount.amount as number) * (rates[amount.currency] || 1);
+	const limAmount = (lAmount.amount as number) * (rates[lAmount.currency] || 1);
+	const lNumber = toNumber(valAmount, limAmount, limitFunc);
+	return lNumber === valAmount ? amount : lAmount;
+};
+
+export const getComparableValue = (
+	{ valuePath, rates }: { valuePath?: string; rates?: Rates },
+	item: Item,
+): number | undefined => {
+	if (item == null) {
+		return;
+	}
+
+	let value: unknown = item;
+
+	if (valuePath != null) {
+		value = get(item, valuePath);
+	}
+	const amountValue = toAmount(rates, value);
+
+	if (amountValue == null) {
+		return;
+	}
+	const amount = toNumber(amountValue.amount);
+
+	if (rates == null || amount == null) {
+		return amount;
+	}
+
+	return amount * (rates[amountValue.currency] || 1);
+};
+
+export const applySingleFilter =
+	(column: FilterColumn, filter: FilterValue) =>
+	(item: Item): boolean => {
+		const value = getComparableValue(column, item);
+
+		if (value == null) {
+			return false;
+		}
+
+		const min = getComparableValue({ ...column, valuePath: 'min' }, filter);
+		const max = getComparableValue({ ...column, valuePath: 'max' }, filter);
+
+		return !(value < (min ?? -Infinity) || value > (max ?? Infinity));
+	};
+
+const formatters: Record<string, Intl.NumberFormat> = {};
+
+export const getFormatter = (
+	currency: string,
+	locale?: string,
+): Intl.NumberFormat => {
+	const id = locale ? locale : '';
+	const key = currency + id || '';
+
+	if (formatters[key]) {
+		return formatters[key];
+	}
+
+	formatters[key] = new Intl.NumberFormat(locale || undefined, {
+		style: 'currency',
+		currency,
+	});
+
+	return formatters[key];
+};
+
+export const renderValue = (
+	rates: Rates | undefined,
+	value: unknown,
+	locale?: string,
+): string => {
+	const amount = toAmount(rates, value);
+	if (amount == null) {
+		return '';
+	}
+	return getFormatter(amount.currency, locale).format(amount.amount as number);
+};
+
+export const getString = (
+	{
+		valuePath,
+		rates,
+		locale,
+	}: { valuePath?: string; rates?: Rates; locale?: string },
+	item: Item,
+): string => {
+	const value = toAmount(rates, valuePath ? get(item, valuePath) : undefined);
+	if (value === undefined) {
+		return '';
+	}
+	if (value === null) {
+		return 'Invalid value';
+	}
+	return renderValue(rates, value, locale);
+};
+
+export const toHashString = (value: AmountValue | null | undefined): string => {
+	if (!value) {
+		return '';
+	}
+	return value.amount + value.currency;
+};
+
+export const fromHashString = (
+	value: string | null | undefined,
+): AmountValue | null | undefined => {
+	if (value == null || value === '') {
+		return;
+	}
+	const params = value.match(/^(-?[\d]+)([\D]+?)$/iu);
+	if (!Array.isArray(params) || params.length < 3) {
+		return null;
+	}
+	return { amount: params[1], currency: params[2] };
+};
+
+export const getCurrency = (
+	{ valuePath }: { valuePath?: string },
+	item: Item,
+): string | undefined =>
+	valuePath ? get(item, valuePath)?.currency : undefined;
+
+export const getInputString = (
+	{ valuePath }: { valuePath?: string },
+	item: Item,
+): string | number | undefined =>
+	valuePath ? get(item, valuePath)?.amount : undefined;
