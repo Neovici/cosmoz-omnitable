13c73527e747245702a38aaf999ebdf3a6c1e8da
John <codingjohn@outlook.com>
2025-10-01 12:54:20 +0800

    feat: convert utils-number.js to typescript

---
+++ src/lib/types.ts
@@ -0,0 +1,51 @@
+export type LimitFunction = (a: number, b: number) => number | undefined;
+
+export type Currency = 'SEK' | 'USD' | 'EUR' | 'AUD';
+
+export interface AmountValue {
+	amount: string | number;
+	currency: Currency | string;
+}
+
+export type ValuePath = 'amount' | 'min' | 'max' | 'value';
+
+export interface Value {
+	valuePath: ValuePath;
+	locale?: string;
+	minimumFractionDigits?: number | null;
+	maximumFractionDigits?: number | null;
+}
+
+export interface AmountObj {
+	amount: AmountValue;
+	bool: boolean;
+	date: Date;
+	dateJSON: string;
+	group: string;
+	id: number;
+	list: string[];
+	name: string;
+	object: {
+		label: string;
+		value: number;
+	};
+	objectList: Array<{
+		name: string;
+		value: number;
+	}>;
+	randomString: string;
+	randomString2: string;
+	randomString3: string;
+	randomString4: string;
+	sub: {
+		subProp: string;
+	};
+	value: number;
+}
+
+export interface FilterObj {
+	min: AmountValue;
+	max: AmountValue;
+}
+
+export type Item = FilterObj | AmountObj;
---
+++ src/lib/utils-number.ts
@@ -0,0 +1,147 @@
+import { get } from '@polymer/polymer/lib/utils/path';
+import { memoooize } from '@neovici/cosmoz-utils/memoize';
+import { LimitFunction, Value, Item, FilterObj } from './types';
+
+export const toNumber = (
+	value?: unknown,
+	limit?: unknown,
+	limitFunc?: LimitFunction,
+): number | undefined => {
+	if (value == null || value === '') {
+		return;
+	}
+
+	const number = typeof value === 'number' ? value : Number(value);
+
+	if (Number.isNaN(number)) {
+		return;
+	}
+
+	if (limitFunc == null || limit == null) {
+		return number;
+	}
+
+	const lNumber = toNumber(limit);
+
+	if (lNumber == null) {
+		return number;
+	}
+
+	return limitFunc(number, lNumber);
+};
+
+export const toInputString = (value?: unknown): string | null => {
+	const val = toNumber(value);
+
+	if (val == null) {
+		return null;
+	}
+
+	return val.toString();
+};
+
+export const getInputString = (
+	{ valuePath }: Pick<Value, 'valuePath'>,
+	item: Item,
+): string | null => {
+	const value = toNumber(valuePath ? get(item, valuePath) : undefined);
+
+	console.log({ item });
+
+	return toInputString(value);
+};
+
+export const toHashString = (value: unknown): string => {
+	const string = toInputString(value);
+
+	if (string == null) {
+		return '';
+	}
+
+	return string;
+};
+
+export const getComparableValue = (
+	{ valuePath, maximumFractionDigits }: Value,
+	item?: Item | FilterObj,
+): number | undefined => {
+	if (item == null || valuePath == null) {
+		return;
+	}
+
+	const value = get(item, valuePath);
+	const numValue = toNumber(value);
+
+	if (numValue == null) {
+		return;
+	}
+
+	if (maximumFractionDigits !== null) {
+		return toNumber(numValue.toFixed(maximumFractionDigits));
+	}
+
+	return numValue;
+};
+
+export const makeFormatter = memoooize(
+	(
+		locale?: string,
+		minimumFractionDigits?: number | null,
+		maximumFractionDigits?: number | null,
+	): Intl.NumberFormat => {
+		const options: Intl.NumberFormatOptions = {
+			localeMatcher: 'best fit', // chrome expects this when using custom options
+		};
+
+		if (minimumFractionDigits !== null) {
+			options.minimumFractionDigits = minimumFractionDigits;
+		}
+
+		if (maximumFractionDigits !== null) {
+			options.maximumFractionDigits = maximumFractionDigits;
+		}
+
+		return new Intl.NumberFormat(locale || undefined, options);
+	},
+);
+
+export const getString = (
+	{ valuePath, locale, minimumFractionDigits, maximumFractionDigits }: Value,
+	item: Item,
+): string | undefined => {
+	const value = get(item, valuePath);
+
+	if (value == null) {
+		return '';
+	}
+
+	const number = toNumber(value);
+
+	if (number == null) {
+		return;
+	}
+
+	const formatter = makeFormatter(
+		locale,
+		minimumFractionDigits,
+		maximumFractionDigits,
+	);
+
+	return formatter.format(number);
+};
+
+export const applySingleFilter =
+	(column: Value, filter: FilterObj) =>
+	(item: Item): boolean => {
+		console.log({ column, item, filter });
+		const value = getComparableValue(column, item);
+
+		if (value == null) {
+			return false;
+		}
+
+		const min = getComparableValue({ ...column, valuePath: 'min' }, filter);
+		const max = getComparableValue({ ...column, valuePath: 'max' }, filter);
+
+		return !(value < (min ?? -Infinity) || value > (max ?? Infinity));
+	};
