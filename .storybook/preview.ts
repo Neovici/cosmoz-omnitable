e7c7bdff6b0869e33d8a5edadea4364b027f1de0
Husoschi Ionut-Catalin <ionuthusoschi@gmail.com>
2025-10-14 23:46:19 +0300

    docs: migrate full-demo page to storybook (#721)
    * feat: migrate full-demo page to storybook
    
    * fix: regenerate package-lock with CI env npm version

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
-/** @type { import('@storybook/web-components').Preview } */
-const preview = {
+import type { Preview } from '@storybook/web-components-vite';
+
+const preview: Preview = {
 	parameters: {
 		actions: { argTypesRegex: '^on[A-Z].*' },
 		controls: {
