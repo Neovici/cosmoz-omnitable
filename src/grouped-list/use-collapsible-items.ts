4b54873080820e59eeba635f0ea4813e16afe0a9
John <codingjohn@outlook.com>
2025-10-15 13:40:08 +0800

    feat: covert use-collapsible-items to ts

--- src/grouped-list/use-collapsible-items.ts
+++ src/grouped-list/use-collapsible-items.ts
@@ -1,10 +1,11 @@
 import { useCallback } from '@pionjs/pion';
 import { useWeakState } from './use-weak-state';
 import { isGroup } from './utils';
+import { Item } from '../lib/types';
 
 export const useCollapsibleItems = () => {
 	const { setItemState, state, signal } = useWeakState(),
-		toggleFold = useCallback((item, folded) => {
+		toggleFold = useCallback((item: Item, folded?: boolean) => {
 			if (!isGroup(item)) {
 				return;
 			}
@@ -13,7 +14,7 @@ export const useCollapsibleItems = () => {
 				folded: folded !== undefined ? folded : !state.folded,
 			}));
 		}, []),
-		toggleCollapse = useCallback((item, collapsed) => {
+		toggleCollapse = useCallback((item: Item, collapsed?: boolean) => {
 			if (isGroup(item)) {
 				return;
 			}
