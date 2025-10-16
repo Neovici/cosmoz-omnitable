6c10b738c7cf32aebba7b2ea1f5760644f316dff
John <codingjohn@outlook.com>
2025-10-16 14:53:22 +0800

    feat: convert cosmoz-grouped-list.js to ts

--- src/grouped-list/cosmoz-grouped-list.ts
+++ src/grouped-list/cosmoz-grouped-list.ts
@@ -3,8 +3,9 @@ import {
 	useCosmozGroupedList,
 } from './use-cosmoz-grouped-list';
 import { component } from '@pionjs/pion';
+import type { UseCosmozGroupedListHost } from './use-cosmoz-grouped-list.js';
 
-const CosmozGroupedList = (host) =>
+const CosmozGroupedList = (host: UseCosmozGroupedListHost) =>
 	renderCosmozGroupedList(useCosmozGroupedList(host));
 
 customElements.define(
