5c017aba6b715b750f19bd623a444cba869d8f9d
John <codingjohn@outlook.com>
2025-10-21 13:17:24 +0800

    feat: convert local.js to ts from drivers directory

--- src/lib/settings/drivers/local.ts
+++ src/lib/settings/drivers/local.ts
@@ -1,18 +1,27 @@
 export default ({ prefix = 'omnitable-' } = {}) => {
-	const read = async (settingsId) => {
+	const read = async (settingsId: string) => {
 		if (!settingsId) {
 			return;
 		}
+
 		try {
-			return JSON.parse(localStorage.getItem(prefix + settingsId));
+			const item = localStorage.getItem(prefix + settingsId);
+
+			if (item == null) {
+				return;
+			}
+
+			return JSON.parse(item);
 		} catch (e) {
 			// eslint-disable-next-line no-console
 			console.error(e);
 		}
 	};
+
 	return {
-		write: async (settingsId, settings) => {
+		write: async (settingsId: string, settings: string) => {
 			const key = prefix + settingsId;
+
 			try {
 				if (settings) {
 					localStorage.setItem(key, JSON.stringify(settings));
