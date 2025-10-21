6c88c3109d76ff5185cd11246e5316421502dccc
John <codingjohn@outlook.com>
2025-10-21 14:33:32 +0800

    feat: convert use-weak-state to ts (#720)

--- src/grouped-list/use-weak-state.ts
+++ src/grouped-list/use-weak-state.ts
@@ -1,10 +1,14 @@
 import { useCallback, useState } from '@pionjs/pion';
-import { callFn, getItemState } from './utils';
+import { callFn, getItemState, ItemsState, ItemState } from './utils';
+import { Item } from '../lib/types';
 
 export const useWeakState = () => {
-	const [wrapper, setState] = useState(() => [new WeakMap()]),
+	const [wrapper, setState] = useState<[ItemsState]>(() => [new WeakMap()]),
 		setItemState = useCallback(
-			(item, newItemState) =>
+			(
+				item: Item,
+				newItemState: ItemState | ((state: ItemState) => ItemState),
+			) =>
 				setState(([state]) => {
 					const itemState = getItemState(item, state);
 					Object.assign(itemState, callFn(newItemState, itemState));
