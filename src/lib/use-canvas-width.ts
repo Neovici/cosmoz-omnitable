5f6a1f25b6a00466882e9677802d96ed0082ec99
John <codingjohn@outlook.com>
2025-10-22 15:40:50 +0800

    feat: convert use-canvas-width.js to ts (#741)

--- src/lib/use-canvas-width.ts
+++ src/lib/use-canvas-width.ts
@@ -1,7 +1,7 @@
 import { useState } from '@pionjs/pion';
 import { useTrackSize } from './use-track-size';
 
-export const useCanvasWidth = (host) => {
+export const useCanvasWidth = (host: HTMLElement) => {
 	const [canvasWidth, setCanvasWidth] = useState(
 		() => host.getBoundingClientRect().width,
 	);
