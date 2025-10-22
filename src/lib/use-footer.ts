312ba135cf01be3799794ad25ec2d415a4429b63
John <codingjohn@outlook.com>
2025-10-22 15:41:50 +0800

    fix: convert use-footer.js to ts (#742)
    * feat: convert use-footer.js to ts
    
    * fix(lint): remove logger

---
+++ src/lib/use-footer.ts
@@ -0,0 +1,28 @@
+interface FooterHost extends HTMLElement {
+	csvFilename?: string;
+	xlsxFilename?: string;
+	xlsxSheetname?: string;
+	topPlacement?: string;
+}
+
+interface UseFooterParams {
+	host: FooterHost;
+	[key: string]: unknown;
+}
+
+export const useFooter = ({ host, ...rest }: UseFooterParams) => {
+	const {
+		csvFilename = 'omnitable.csv',
+		xlsxFilename = 'omnitable.xlsx',
+		xlsxSheetname = 'Omnitable',
+		topPlacement = 'top-end',
+	} = host;
+
+	return {
+		csvFilename,
+		xlsxFilename,
+		xlsxSheetname,
+		topPlacement,
+		...rest,
+	};
+};
