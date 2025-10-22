c1d39154e83768edbade9073d86c01d1c33dbab7
John <codingjohn@outlook.com>
2025-10-22 14:47:07 +0800

    fix: upgrade number range input (#675)
    * feat: create dropdown menu for omnitable headers
    
    Replaced the paper-dropdown-menu on cosmoz-omnitable-number-range-input first
    
    * refactor: reuse cosmoz-dropdown hooks
    
    * fix: fix focus issue on cosmoz-input
    
    * chore: move cosmoz-omnitable-dropdown-menu directory location
    
    * feat: reuse cosmoz-dropdown
    
    * chore: change filter value slot
    
    * chore: cleanup
    
    * refactor: make dropdown reusable
    
    * fix: fix uncaught properties error
    
    * chore: use lit classMap directive
    
    * fix: fix constant headerFocused
    
    * feat: add horizontalAlign property
    
    * chore: move clear button up
    
    * fix: focus on first input component when dropdown opened
    
    * chore: fix formatting
    
    * refactor: move float inside dropdown
    
    * feat: add externalValues class on dropdown
    
    * chore: revert whitespace change
    
    * chore: remove unnecessary blank line
    
    ---------
    
    Co-authored-by: Cristian Necula <VirusZ@gmail.com>

--- src/lib/cosmoz-omnitable-number-range-input.js
+++ src/lib/cosmoz-omnitable-number-range-input.js
@@ -1,10 +1,10 @@
 import { PolymerElement } from '@polymer/polymer';
-import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { html } from 'lit-html';
 import { _ } from '@neovici/cosmoz-i18next';
 import '@neovici/cosmoz-input';
 import { rangeInputMixin } from './cosmoz-omnitable-range-input-mixin';
 import { polymerHauntedRender } from './polymer-haunted-render-mixin';
+import { renderDropdown } from './cosmoz-omnitable-dropdown';
 
 class NumberRangeInput extends rangeInputMixin(
 	polymerHauntedRender(PolymerElement),
@@ -24,46 +24,18 @@ class NumberRangeInput extends rangeInputMixin(
 				type: String,
 				computed: '_computeFilterText(filter.*, formatter)',
 			},
+			headerFocused: { type: Boolean, value: false },
 		};
 	}
 
 	render() {
 		const onOpenedChanged = (event) => {
-			this.headerFocused = event.detail.value;
+			this.headerFocused = event.type === 'focus';
 			this._onDropdownOpenedChanged(event);
 		};
 
 		return html`
 			<style>
-				paper-dropdown-menu {
-					--iron-icon-width: 0;
-					display: block;
-					text-align: right;
-				}
-
-				.dropdown-content h3 {
-					font-weight: 500;
-					font-size: 13px;
-					margin: 0;
-					font-family: var(
-						--cosmoz-input-font-family,
-						var(--paper-font-subhead_-_font-family, 'Inter', sans-serif)
-					);
-				}
-
-				.dropdown-content {
-					padding: 10px 10px 10px 10px;
-					min-width: 120px;
-					text-align: left;
-					background: var(--cosmoz-omnitable-amount-input-background, #ffffff);
-					border-radius: 6px;
-					backdrop-filter: blur(16px) saturate(180%);
-					-webkit-backdrop-filter: blur(16px) saturate(180%);
-					box-shadow:
-						0 4px 24px 0 rgba(0, 0, 0, 0.18),
-						0 1.5px 6px 0 rgba(0, 0, 0, 0.1);
-				}
-
 				cosmoz-input[type='number'] {
 					background: var(--cosmoz-omnitable-amount-input-background, #ffffff);
 					border-radius: 6px;
@@ -88,17 +60,17 @@ class NumberRangeInput extends rangeInputMixin(
 				@click=${() => this.resetFilter()}
 				?visible=${this.hasFilter()}
 			></cosmoz-clear-button>
-			<paper-dropdown-menu
-				label=${this.title}
-				placeholder=${ifDefined(this._filterText)}
-				class="external-values-${this.externalValues}"
-				title=${this._tooltip}
-				horizontal-align="right"
-				?opened=${this.headerFocused}
-				@opened-changed=${onOpenedChanged}
-			>
-				<div class="dropdown-content" slot="dropdown-content">
-					<h3 style="margin: 0;">${this.title}</h3>
+
+			${renderDropdown({
+				title: this.title,
+				tooltip: this._tooltip,
+				filterText: this._filterText,
+				headerFocused: this.headerFocused,
+				horizontalAlign: 'right',
+				externalValues: this.externalValues,
+				onOpenedChanged,
+				content: html`
+					<h3>${this.title}</h3>
 					<cosmoz-input
 						class=${this._fromClasses}
 						type="number"
@@ -125,8 +97,8 @@ class NumberRangeInput extends rangeInputMixin(
 						min=${this._toInputString(this._limit.toMin)}
 						max=${this._toInputString(this._limit.toMax)}
 					></cosmoz-input>
-				</div>
-			</paper-dropdown-menu>
+				`,
+			})}
 		`;
 	}
 
