56a0303ad1201f0c4be53f98f00ffe5722273c68
John <codingjohn@outlook.com>
2026-01-27 18:00:00 +0800

    Convert use-hash-state.js to typescript (#854)
    * feat: convert use-hashstate.js to ts
    
    * feat: add types for multi-mode codecs
    
    * chore: revert back js code structure
    
    * chore: set useHashState return type as tuple explicitly

---
+++ src/lib/use-hash-state.ts
@@ -0,0 +1,99 @@
+import { navigate } from '@neovici/cosmoz-router';
+import { identity, invoke } from '@neovici/cosmoz-utils/function';
+import {
+	hashUrl,
+	multiParse,
+	singleParse,
+} from '@neovici/cosmoz-utils/location';
+import { useCallback, useState } from '@pionjs/pion';
+
+type Codec = (value: unknown) => string;
+type Reader<T> = (value: string) => T;
+type Parser = <T>(param: string, reader?: Reader<T>) => T | null;
+
+type MultiReadCodec<T> = (entry: [string, string | null]) => [string, T];
+type MultiWriteCodec<T> = (entry: [string, T]) => [string, string | undefined];
+
+type Parameterize = (
+	hashParam: string,
+	value: unknown,
+	codec: Codec,
+	searchParams: URLSearchParams,
+) => void;
+
+interface UseHashStateOptions<T> {
+	suffix?: string;
+	read?: Reader<T> | MultiReadCodec<T>;
+	write?: Codec | MultiWriteCodec<T>;
+	multi?: boolean;
+}
+
+const makeLinker =
+		(parameterize: Parameterize) =>
+		(hashParam: string, value: unknown, codec?: Codec): string => {
+			const url = hashUrl(),
+				searchParams = new URLSearchParams(url.hash.replace('#', ''));
+
+			// TODO: make parameterize pure
+			parameterize(
+				hashParam,
+				value,
+				codec ?? (identity as Codec),
+				searchParams,
+			);
+
+			return (
+				'#!' +
+				Object.assign(url, { hash: searchParams }).href.replace(
+					location.origin,
+					'',
+				)
+			);
+		},
+	isEmpty = (v: unknown): boolean => v == null || v === '',
+	singleLink = makeLinker((hashParam, value, codec, searchParams) =>
+		!isEmpty(codec(value))
+			? searchParams.set(hashParam, codec(value))
+			: searchParams.delete(hashParam),
+	),
+	multiLink = makeLinker((hashParam, value, codec, searchParams) =>
+		Object.entries(value as Record<string, unknown>)
+			.map(codec)
+			.forEach(([key, value]) =>
+				!isEmpty(value)
+					? searchParams.set(hashParam + key, value)
+					: searchParams.delete(hashParam + key),
+			),
+	);
+
+export const useHashState = <T>(
+	initial: T,
+	param?: string,
+	{ suffix = '', read, write, multi }: UseHashStateOptions<T> = {},
+): [T, (state: T | ((prevState: T) => T)) => void] => {
+	const [link, parseHash] = multi
+			? [multiLink, multiParse as Parser]
+			: [singleLink, singleParse as Parser],
+		[state, _setState] = useState<T>(() =>
+			param == null
+				? initial
+				: (parseHash<T>(param + suffix, read as Reader<T>) ?? initial),
+		),
+		setState = useCallback(
+			(state: T | ((prevState: T) => T)) =>
+				_setState((oldState) => {
+					const newState = invoke(state, oldState);
+
+					if (param != null) {
+						navigate(link(param + suffix, newState, write as Codec), null, {
+							notify: false,
+						});
+					}
+
+					return newState;
+				}),
+			[param, suffix, link, write],
+		);
+
+	return [state, setState];
+};
