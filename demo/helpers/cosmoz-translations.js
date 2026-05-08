dfc43d8e0dd4cd7ea683998d0db5b7f02a8ea33c
Cristian Necula <virusz@gmail.com>
2026-05-08 08:06:20 +0000

    feat: migrate list-data and demos from cosmoz-i18next

--- demo/helpers/cosmoz-translations.js
+++ demo/helpers/cosmoz-translations.js
@@ -1,65 +1,94 @@
-import { PolymerElement } from '@polymer/polymer/polymer-element';
-import { html } from '@polymer/polymer/lib/utils/html-tag';
+import i18next from 'i18next';
 
-import { translatable } from '@neovici/cosmoz-i18next';
+const resources = {
+	en: { translation: {} },
+	sv: {
+		translation: {
+			Amount: 'Belopp',
+			Ascending: 'stigande',
+			Descending: 'fallande',
+			'Group on': 'Gruppera pa',
+			Group: 'Grupp',
+			'No grouping': 'Ingen gruppering',
+			'No sorting': 'Ingen sortering',
+			'Sort on': 'Sortera pa',
+			'{0} group_one': '{0} grupp',
+			'{0} group_other': '{0} grupper',
+			'{0} row_one': '{0} rad',
+			'{0} row_other': '{0} rader',
+			'{0} selected item_one': '{0} valt objekt',
+			'{0} selected item_other': '{0} valda objekt',
+		},
+	},
+	fr: {
+		translation: {
+			Amount: 'Montant',
+			Ascending: 'croissant',
+			Descending: 'decroissant',
+			'Group on': 'Grouper sur',
+			Group: 'Groupe',
+			'No grouping': 'Pas de groupement',
+			'No sorting': 'Pas de tri',
+			'Save as CSV': 'Exporter en CSV',
+			'Sort on': 'Trier sur',
+			'{0} group_one': '{0} groupe',
+			'{0} group_other': '{0} groupes',
+			'{0} row_one': '{0} ligne',
+			'{0} row_other': '{0} lignes',
+			'{0} selected item_one': '{0} element selectionne',
+			'{0} selected item_other': '{0} elements selectionnes',
+		},
+	},
+};
 
-class CosmozTranslations extends translatable(PolymerElement) {
-	static get template() {
-		return html`
-			<cosmoz-i18next
-				translations="[[ _getTranslations(locale) ]]"
-				interpolation-prefix="{"
-				interpolation-suffix="}"
-				key-separator="::"
-				ns-separator=":::"
-			>
-			</cosmoz-i18next>
-		`;
+const ensureInitialized = async () => {
+	if (i18next.isInitialized) {
+		return;
 	}
 
-	static get is() {
-		return 'cosmoz-translations';
+	await i18next.init({
+		resources,
+		lng: 'en',
+		fallbackLng: 'en',
+		interpolation: {
+			prefix: '{',
+			suffix: '}',
+		},
+	});
+};
+
+class CosmozTranslations extends HTMLElement {
+	static get observedAttributes() {
+		return ['locale'];
 	}
 
-	_getTranslations(locale) {
-		if (locale === 'sv') {
-			return {
-				Amount: 'Belopp',
-				Ascending: 'stigande',
-				Descending: 'fallande',
-				'Group on': 'Gruppera på',
-				Group: 'Grupp',
-				'No grouping': 'Ingen gruppering',
-				'No sorting': 'Ingen sortering',
-				'Sort on': 'Sortera på',
-				'{0} group': '{0} grupp',
-				'{0} group_plural': '{0} grupper',
-				'{0} row': '{0} rad',
-				'{0} row_plural': '{0} rader',
-				'{0} selected item': '{0} valt objekt',
-				'{0} selected item_plural': '{0} valda objekt',
-			};
-		} else if (locale === 'fr') {
-			return {
-				Amount: 'Montant',
-				Ascending: 'croissant',
-				Descending: 'décroissant',
-				'Group on': 'Grouper sur',
-				Group: 'Groupe',
-				'No grouping': 'Pas de groupement',
-				'No sorting': 'Pas de tri',
-				'Save as CSV': 'Eporter en CSV',
-				'Sort on': 'Trier sur',
-				'{0} group': '{0} groupe',
-				'{0} group_plural': '{0} groupes',
-				'{0} row': '{0} ligne',
-				'{0} row_plural': '{0} lignes',
-				'{0} selected item': '{0} élément sélectionné',
-				'{0} selected item_plural': '{0} éléments sélectionnés',
-			};
+	connectedCallback() {
+		void this.#setLocale(this.locale);
+	}
+
+	attributeChangedCallback(name, _oldValue, newValue) {
+		if (name === 'locale') {
+			void this.#setLocale(newValue);
+		}
+	}
+
+	get locale() {
+		return this.getAttribute('locale');
+	}
+
+	set locale(value) {
+		if (value == null) {
+			this.removeAttribute('locale');
+			return;
 		}
 
-		return {};
+		this.setAttribute('locale', value);
+	}
+
+	async #setLocale(locale) {
+		await ensureInitialized();
+		await i18next.changeLanguage(locale || 'en');
 	}
 }
-customElements.define(CosmozTranslations.is, CosmozTranslations);
+
+customElements.define('cosmoz-translations', CosmozTranslations);
